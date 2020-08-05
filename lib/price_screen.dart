7e07c3ea6c5fe51d1681faa5341c128af2f3381f
apphow <apehow111@gmail.com>
2020-08-04 23:48:51 -0400

    Cleaned up code by deleting getPicker method and adding a ternary operator instead

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -55,14 +55,6 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
-  Widget getPicker() {
-    if (Platform.isIOS) {
-      return iOSPicker();
-    } else if (Platform.isAndroid) {
-      return androidDropdownButton();
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -99,7 +91,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: getPicker(),
+            child: Platfor.isIOS ? iOSPicker() : androidDropdownButton(),
           ),
         ],
       ),
